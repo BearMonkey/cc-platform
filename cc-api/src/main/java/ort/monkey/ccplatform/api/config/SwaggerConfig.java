453318dc44db2dc912ef9d2e4c24acb9e2b71200
chenchuan <chenchuan@xxx.com>
2024-05-20 17:19:14 +0800

    nacos 配置管理

--- cc-api/src/main/java/ort/monkey/ccplatform/api/config/SwaggerConfig.java
+++ cc-api/src/main/java/ort/monkey/ccplatform/api/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.monkey.ccplatform.demo.config;
+package ort.monkey.ccplatform.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
