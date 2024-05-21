1057fd2021894f82ed15102067fc2e6697b07a65
chenchuan <chenchuan@xxx.com>
2024-05-21 10:51:12 +0800

    使用bootstrap方式获取nacos配置

--- cc-demo/src/main/java/org/monkey/ccplatform/demo/controller/DemoController.java
+++ cc-demo/src/main/java/org/monkey/ccplatform/demo/controller/DemoController.java
@@ -3,6 +3,7 @@ package org.monkey.ccplatform.demo.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,14 @@ import ort.monkey.ccplatform.api.dto.CcOrderDto;
 @Api(value = "demo接口", tags = "/demo")
 public class DemoController {
 
+    @Value("${org.monkey.name:Ashe}")
+    private String name;
+
     @ApiOperation(value = "/say", httpMethod = "GET")
     @ApiParam("无")
     @GetMapping("/say")
     public String hello() {
-        return "Hello World!";
+        return "Hello World, " + name;
     }
 
     @ApiOperation(value = "/add", httpMethod = "POST")
